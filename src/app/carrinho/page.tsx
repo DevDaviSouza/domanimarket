54126ee74401c89d810e1d6e22e606506fd62603
Davi Souza <davii.clink@gmail.com>
2025-08-03 19:20:23 -0300

    feat: aprimoramento do design do carrinho

--- src/app/carrinho/page.tsx
+++ src/app/carrinho/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Carrinho() {
   return (
     <div className="flex flex-col items-center lg:flex-row justify-center gap-y-7  py-10 lg:space-x-5">
-      <div className="flex flex-col lg:w-3/5 p-4 space-y-6 bg-neutral-200 rounded-2xl items-center lg:items-start text-[13px] lg:text-base">
+      <div className="flex flex-col lg:w-3/5 p-4 space-y-6 bg-neutral-200 rounded-2xl items-center lg:items-start text-[13px] lg:text-base shadow-[7px_7px_10px_rgba(0,0,0,0.7)]">
         <table  className="lg:w-full text-center items-center justify-center gap-5 w-72">
           <thead className="border-b-2 border-neutral-400">
             <tr>
@@ -33,7 +33,7 @@ export default function Carrinho() {
         <BotaoPadrao colorButton="bg-red-800" fontColor="white" text="Limpar carrinho"/>
       </div>
       
-      <div className="flex flex-col lg:w-1/5  px-7 py-4 space-y-2 bg-neutral-200 items-center text-center rounded-2xl w-72">
+      <div className="flex flex-col lg:w-1/5  px-7 py-4 space-y-2 bg-neutral-200 items-center text-center rounded-2xl w-72 shadow-[7px_7px_10px_rgba(0,0,0,0.7)]">
         <h2 className="text-2xl lg:text-3xl">Resumo</h2>
         <h3 className="text-xl lg:text-2xl">Total: R$400,00</h3>
         <h4 className="text">Qtd. Itens: 2</h4>
